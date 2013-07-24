6915d05eb81bcd2d5f4d563e23eb2b09fca2520b
Alex Agulyansky <alex.agulyansky@quickmobile.com>
2013-07-24 09:33:31 +0000

    Extensive testing for everything except fetch cache. TBD...

--- lib/Arnm/Cache/AntiDogPileDriver.php
+++ lib/Arnm/Cache/AntiDogPileDriver.php
@@ -4,30 +4,38 @@ namespace Arnm\Cache;
 use Doctrine\Common\Cache\Cache;
 /**
  * This wrapper is built to prevent Dog-pile effect in caching
- * 
+ *
  * @author Alex Agulyansky <alex@iibspro.com>
  * @copyright Copyright (c) 2013, IIB Solutions Ltd.
  */
 class AntiDogPileDriver implements Cache
 {
-	const MAIN_SUFFIX = ':main';
-	const MAIN_LIFETIME = 86400;
+	const STALE_SUFFIX = ':stale';
+	const STALE_LIFETIME = 86400;
 	const LOCK_SUFFIX = ':lock';
-	const LOCK_LIFETIEM = 10;
-	
+	const LOCK_LIFETIME = 10;
+
 	/**
 	 * @var Cache
 	 */
 	private $provider;
-	
+
 	/**
 	 * {@inheritdoc}
 	 * @see Doctrine\Common\Cache.Cache::contains()
 	 */
     public function contains($key)
     {
-        // TODO Auto-generated method stub
-        
+        $provider = $this->getProvider();
+
+        $contains = $provider->contains($key);
+        //check is anyone trys to regenerate this cache at the moment
+        //if not, then the cache HAS in fact expired
+        if(!$contains && $this->isLocked($key)) {
+            $contains = $provider->contains($this->getStaleKey($key));
+        }
+
+        return $contains;
     }
 
 	/**
@@ -36,8 +44,14 @@ class AntiDogPileDriver implements Cache
 	 */
     public function delete($key)
     {
-        // TODO Auto-generated method stub
-        
+        $provider = $this->getProvider();
+        $deleted = $provider->delete($key);
+        //we do not care if stale was deleted or not in this case
+        $provider->delete($this->getStaleKey($key));
+        //also make sure that the lock is deleted
+        $this->unlock($key);
+
+        return $deleted;
     }
 
 	/**
@@ -47,7 +61,7 @@ class AntiDogPileDriver implements Cache
     public function fetch($key)
     {
         // TODO Auto-generated method stub
-        
+
     }
 
 	/**
@@ -65,15 +79,34 @@ class AntiDogPileDriver implements Cache
 	 */
     public function save($key, $data, $lifeTime = 0)
     {
-		return $this->getProvider()->save($key, $data, $lifeTime);        
+        $provider = $this->getProvider();
+
+        //set main cache
+		$saved = $provider->save($key, $data, $lifeTime);
+		$staleSaved = false;
+		if($saved) {
+		    $staleSaved = $provider->save($this->getStaleKey($key), $data, $this->getStaleLifetime($lifeTime));
+		}
+
+		//small backup plan
+		//if something has not been saved we delete everything and letting the client know about the failure
+		if(!$saved || !$staleSaved) {
+		    $this->delete($key);
+		    return false;
+		}
+
+		//if everything is OK, make sure that we unlocked the key
+		$this->unlock($key);
+
+		return true;
     }
-    
+
     /**
      * Provide defaul proxy methid for allthe rest of the method that have not been specifically implemented
-     * 
+     *
      * @param string $name
      * @param array $arguments
-     * 
+     *
      * @return mixed
      */
     public function __call($name, $arguments)
@@ -83,26 +116,94 @@ class AntiDogPileDriver implements Cache
     	{
     		throw new \RuntimeException("Call to undefined method ".__CLASS__.":".$name);
     	}
-    	
+
     	return call_user_func_array(array($provider, $name), $arguments);
     }
-    
+
+    /**
+     * Checks if the cache regeneration for this key is locked.
+     *
+     * @param string $key
+     *
+     * @return boolean
+     */
+    public function isLocked($key)
+    {
+        return $this->getProvider()->contains($this->getLockKey($key));
+    }
+
+    /**
+     * Lockes the key for limited time
+     *
+     * @param string $key
+     *
+     * @return boolean
+     */
+    public function lock($key)
+    {
+        return $this->getProvider()->save($this->getLockKey($key), 1, self::LOCK_LIFETIME);
+    }
+
+    /**
+     * Unlockes the key
+     *
+     * @param string $key
+     *
+     * @return boolean
+     */
+    public function unlock($key)
+    {
+        return $this->getProvider()->delete($this->getLockKey($key));
+    }
+
+    /**
+     * Creates a cache ley for stale cache
+     *
+     * @param string $key
+     */
+    protected function getStaleKey($key)
+    {
+        return ((string) $key.self::STALE_SUFFIX);
+    }
+
+    /**
+     * Calculates the lifetime for stale cache
+     *
+     * @param int $lifetime
+     *
+     * @return int
+     */
+    protected function getStaleLifetime($lifetime)
+    {
+        return ((int) ($lifetime+self::STALE_LIFETIME));
+    }
+
+    /**
+     * Creates a cache ley for locking
+     *
+     * @param string $key
+     */
+    protected function getLockKey($key)
+    {
+        return ((string) $key.self::LOCK_SUFFIX);
+    }
+
     /**
      * Sets cache provider
-     * 
+     *
      * @param Cache $cache
      */
     public function setProvider(Cache $cache)
     {
     	$this->provider = $cache;
     }
-    
+
     /**
      * Gets cache provider object
-     * 
+     *
      * @return Cache
      */
-    public function getProvider() 
+    public function getProvider()
     {
     	return $this->provider;
     }
--- test/Arnm/Cache/AntiDogPileDriverTest.php
+++ test/Arnm/Cache/AntiDogPileDriverTest.php
@@ -7,27 +7,35 @@ use Arnm\Cache\AntiDogPileDriver;
  */
 class AntiDogPileDriverTest extends \PHPUnit_Framework_TestCase
 {
-    
+
 	/**
 	 * Tests save and getStats methods
 	 */
     public function testSaveAndGetStats()
     {
-        $cache = $this->getMock('Doctrine\Common\Cache\ArrayCache', array('save', 'getStats'), array(), '', false);
-        $cache->expects($this->once())
+        $cache = $this->getMock('Doctrine\Common\Cache\ArrayCache', array('save', 'delete', 'getStats'), array(), '', false);
+        $cache->expects($this->at(0))
             ->method('save')
             ->with($this->equalTo('key'), $this->equalTo('value'), 30)
+            ->will($this->returnValue(true));
+        $cache->expects($this->at(1))
+            ->method('save')
+            ->with($this->equalTo('key'.AntiDogPileDriver::STALE_SUFFIX), $this->equalTo('value'), (30+AntiDogPileDriver::STALE_LIFETIME))
+            ->will($this->returnValue(true));
+        $cache->expects($this->once())
+            ->method('delete')
+            ->with($this->equalTo('key'.AntiDogPileDriver::LOCK_SUFFIX))
             ->will($this->returnValue(true));
         $cache->expects($this->once())
             ->method('getStats')
             ->will($this->returnValue('stats'));
-            
+
         $wrapper = new AntiDogPileDriver();
         $wrapper->setProvider($cache);
         $this->assertTrue($wrapper->save('key', 'value', 30));
         $this->assertEquals('stats', $wrapper->getStats());
     }
-    
+
     /**
      * Tests proxying undefined methods
      */
@@ -38,17 +46,112 @@ class AntiDogPileDriverTest extends \PHPUnit_Framework_TestCase
             ->method('call')
             ->with($this->equalTo('input'))
             ->will($this->returnValue('called'));
-            
+
         $wrapper = new AntiDogPileDriver();
         $wrapper->setProvider($cache);
         $this->assertEquals('called', $wrapper->call('input'));
-        
+
         try {
         	$wrapper->hello();
         	$this->fail("Should through RuntimeException if the method is not defined!");
         } catch (\RuntimeException $e){
-        	
+
         }
+    }
+
+    public function testDelete()
+    {
+        $cache = $this->getMock('Doctrine\Common\Cache\ArrayCache', array('delete'), array(), '', false);
+        $cache->expects($this->at(0))
+            ->method('delete')
+            ->with($this->equalTo('key'))
+            ->will($this->returnValue(true));
+        $cache->expects($this->at(1))
+            ->method('delete')
+            ->with($this->equalTo('key'.AntiDogPileDriver::STALE_SUFFIX))
+            ->will($this->returnValue(true));
+        $cache->expects($this->at(2))
+            ->method('delete')
+            ->with($this->equalTo('key'.AntiDogPileDriver::LOCK_SUFFIX))
+            ->will($this->returnValue(true));
+        $cache->expects($this->at(3))
+            ->method('delete')
+            ->with($this->equalTo('key'))
+            ->will($this->returnValue(false));
+        $cache->expects($this->at(4))
+            ->method('delete')
+            ->with($this->equalTo('key'.AntiDogPileDriver::STALE_SUFFIX))
+            ->will($this->returnValue(true));
+        $cache->expects($this->at(5))
+            ->method('delete')
+            ->with($this->equalTo('key'.AntiDogPileDriver::LOCK_SUFFIX))
+            ->will($this->returnValue(true));
+
+        $wrapper = new AntiDogPileDriver();
+        $wrapper->setProvider($cache);
+        $this->assertTrue($wrapper->delete('key'));
+        $this->assertFalse($wrapper->delete('key'));
+    }
+
+    public function testContains()
+    {
+        $cache = $this->getMock('Doctrine\Common\Cache\ArrayCache', array('contains'), array(), '', false);
+        //in case that the main one exists and valid
+        $cache->expects($this->at(0))
+            ->method('contains')
+            ->with($this->equalTo('key'))
+            ->will($this->returnValue(true));
+        //in case that the main has expired and there is nobody else regenerating the cache
+        $cache->expects($this->at(1))
+            ->method('contains')
+            ->with($this->equalTo('key'))
+            ->will($this->returnValue(false));
+        $cache->expects($this->at(2))
+            ->method('contains')
+            ->with($this->equalTo('key'.AntiDogPileDriver::LOCK_SUFFIX))
+            ->will($this->returnValue(false));
+        //in case that the main has expired, there is somebody else trying to regenerate the cache and stale still valid
+        $cache->expects($this->at(3))
+            ->method('contains')
+            ->with($this->equalTo('key'))
+            ->will($this->returnValue(false));
+        $cache->expects($this->at(4))
+            ->method('contains')
+            ->with($this->equalTo('key'.AntiDogPileDriver::LOCK_SUFFIX))
+            ->will($this->returnValue(true));
+        $cache->expects($this->at(5))
+            ->method('contains')
+            ->with($this->equalTo('key'.AntiDogPileDriver::STALE_SUFFIX))
+            ->will($this->returnValue(true));
+
+        $wrapper = new AntiDogPileDriver();
+        $wrapper->setProvider($cache);
+        //in case that the main one exists and valid
+        $this->assertTrue($wrapper->contains('key'));
+        //in case that the main has expired and there is nobody else regenerating the cache
+        $this->assertFalse($wrapper->contains('key'));
+        //in case that the main has expired, there is somebody else trying to regenerate the cache and stale still valid
+        //in this case we expect to get the stale value and let the one that regenerates the cache finish his job
+        $this->assertTrue($wrapper->contains('key'));
+    }
+
+    public function testLockUnlock()
+    {
+        $cache = $this->getMock('Doctrine\Common\Cache\ArrayCache', array('save', 'delete'), array(), '', false);
+        $cache->expects($this->once())
+            ->method('save')
+            ->with($this->equalTo('lock'.AntiDogPileDriver::LOCK_SUFFIX), $this->equalTo(1), $this->equalTo(AntiDogPileDriver::LOCK_LIFETIME))
+            ->will($this->returnValue(true));
+        $cache->expects($this->once())
+            ->method('delete')
+            ->with($this->equalTo('lock'.AntiDogPileDriver::LOCK_SUFFIX))
+            ->will($this->returnValue(true));
+
+        $wrapper = new AntiDogPileDriver();
+        $wrapper->setProvider($cache);
+
+        $wrapper->lock('lock');
+        $wrapper->unlock('lock');
     }
 }
 
